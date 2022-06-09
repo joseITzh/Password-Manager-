6c6522acd588268e8fc020e52cce4ec5bbdd7a15
Jose Carlos Parada <99294100+joseITzh@users.noreply.github.com>
2022-06-09 23:32:36 +0200

    Update main.py
    What is new?
    - Variable names have been changed to more accurate names.
    - I have added a button that will search for the email and password with the given website name.  
    - try, except, else, and finally pattern for two functions.

--- main.py
+++ main.py
@@ -36,62 +36,112 @@ def generate_password():
     pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    entry_website = website.get()
-    entry_email = email.get()
-    entry_password = password_entry.get()
-    #This message box returns a boolean value, if you press "ok", then the result will be true.
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
 
-    if len(entry_website) == 0 or len(entry_password) == 0:
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }}
+
+    #This message box returns a boolean value, if you press "ok", then the result will be true.
+    if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(message="Don't leave any fields empty!!!")
     else:
-        is_ok =messagebox.askokcancel(title=entry_website, message=f"These are the details entered: \nEmail: {entry_email} \nPassword: {entry_password} \nIs it okey to save?")
+        #"a" means that it will be appended to the end of the file.
+        #When you open a file with "r" mode, the file will be read.
+        #When you open a file in "w" mode, if the file doesn't exist, it will be created.
 
-    if is_ok:
-        # If file doesn't exist, it will be created.
-        # "a" means that it will be appended to the end of the file.
-        data = open("data.txt", "a")
-        data.write(f"{entry_website} | {entry_email} | {entry_password} \n")
-        #With .delete(0, END) the previously prompted information in the entries will be cleaned.
-        website.delete(0, END)
-        password_entry.delete(0, END)
+        #To add information to a json file you need to read it first and then you can update it.
 
-# ---------------------------- UI SETUP ------------------------------- #
+        #Open the file.
+        try:
+            # Try opening the file and try reading the data inside.
+            with open("data.json", "r") as data_file:
+                # Reading old data.
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                # We are dumping the new_data (first argument) nested dictionary.
+                # The second argument is the data file we want to put it into.
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            # If file was found, we will get hold of the data we got on the try block.
+            # Updating old data with new data.
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                # We are dumping the new_data (first argument) nested dictionary.
+                # The second argument is the data file we want to put it into.
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            #If we print "data", we get a nested dictionary.
+            data = json.load(data_file)
+    except FileNotFoundError:
+        #In case there is no json file.
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        #Checking if the key is in the dictionary.
+        if website_entry in data:
+            #It is a nested dictionary... looks like this when you access it...
+            email = data[website_entry]["email"]
+            password = data[website_entry]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+    # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 #Size of whole window.
-canvas = Canvas(width=200, height=200)
+canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 #X and Y position for image.
 canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-canvas.grid(row=1, column=2)
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-label1 = Label(text="Website: ")
-label1.grid(row=2, column=1)
-website = Entry(width=35)
-website.grid(row=2, column=2, columnspan=2)
+#Entries
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 #The cursor will be in the entry widget.
-website.focus()
-
-label2 = Label(text="Email/ Username: ")
-label2.grid(row=3, column=1)
-email = Entry(width=35)
-email.grid(row=3, column=2, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
 #The 0 means that you will start typing at the index 0.
-email.insert(0, string="abogangsterbesode3@gmail.com")
-
-label3 = Label(text="Password: ")
-label3.grid(row=4, column=1)
+email_entry.insert(0, "angela@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=4, column=2)
+password_entry.grid(row=3, column=1)
 
-gen_pass = Button(text="Generate Password", command=generate_password)
-gen_pass.grid(row=4, column=3)
-
-add = Button(width=36, text="Add", command=save)
-add.grid(row=5, column=2, columnspan=2)
-
+# Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
